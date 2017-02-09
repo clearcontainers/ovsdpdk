3e355e730905bf541803695c97d9bcf18e8d03cb
Manohar Castelino <manohar.r.castelino@intel.com>
2017-02-08 18:15:02 -0800

    Initial Commit: Setup the base framework for the plugin
    The TODO's need to be addressed to used this
    
    Signed-off-by: Manohar Castelino <manohar.r.castelino@intel.com>
    
    Signed-off-by: Manohar Castelino <manohar.r.castelino@intel.com>

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# ovsdpdk
-Docker Network Plugin for ovs-dpdk
+# ovs dpdk with Clear Containers
+
+This is simple standalone Docker Plugin implementation to demonstrate Clear Containers with ovs dpdk.
+
+For more details about Clear Containers
+https://github.com/01org/cc-oci-runtime
+https://clearlinux.org/clear-containers
+
+For more information about ovs dpdk
+http://dpdk.org/
+
+
+The docker plugin is used to setup and manage ovs dpdk to establish connections between two
+clear containers running on the same machine
+
+# How to use this plugin
+
+
+0. Build this plugin. 
+
+        go build
+
+1. Ensure that your plugin is discoverable https://docs.docker.com/engine/extend/plugin_api/#/plugin-discovery
+
+        sudo cp ovsdpdk.json /etc/docker/plugins
+
+
+2. Start the plugin
+
+        sudo ./ovsdpdk &
+        
+   Note: Enable password less sudo to ensure the plugin will run in the background without prompting.
+
+3. Try ovsdpdk connectivity between Clear Containers
+
+	This example demonstrates the ovs-dpdk example as illustrated by
+	https://software.intel.com/en-us/articles/using-open-vswitch-with-dpdk-for-inter-vm-nfv-applications
+
+
+        #Perform cleanup and setup the host to support ovsdpdk
+
+
+        #Create the ovsdpdk based container networks using the custom ovsdpdk docker driver
+        sudo docker network create -d=ovsdpdk --ipam-driver=ovsdpdk --subnet=192.168.1.0/24 --gateway=192.168.1.1  --opt "bridge"="ovsbr" ovsdpdk_net
+
+        #Create a docker containers one on each network using the clear container runtime
+        #The IP address of each container is specified
+        sudo docker run -d --net=ovsdpdk_net --ip=192.168.1.2 --mac-address=CA:FE:CA:FE:01:02 --name "ccovs1" debian bash -c "ip a; ip route; sleep 30000"
+
+        #Test network connectivity between the two containers over the ovs dpdk based network
+        sudo docker run --net=ovsdpdk_net --ip=192.168.1.3 --mac-address=CA:FE:CA:FE:01:03 --name "ccovs2" debian bash -c "ip a; ip route; ping 192.168.1.2"
+
+        #Cleanup
+        sudo docker kill `sudo docker ps --no-trunc -aq` ; sudo docker rm `sudo docker ps --no-trunc -aq`
+        sudo docker network rm ovsdpdk_net 
---
+++ plugin.go
@@ -0,0 +1,783 @@
+//
+// Copyright (c) 2017 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os/exec"
+	"sync"
+	"time"
+
+	"github.com/01org/ciao/ssntp/uuid"
+	"github.com/boltdb/bolt"
+	"github.com/docker/libnetwork/drivers/remote/api"
+	ipamapi "github.com/docker/libnetwork/ipams/remote/api"
+	"github.com/golang/glog"
+	"github.com/gorilla/mux"
+)
+
+type epVal struct {
+	IP      string
+	ovsPort string //The ovs dpdk vhost user port
+}
+
+type nwVal struct {
+	Bridge  string //The ovs bridge on which the ports will eb created
+	Gateway net.IPNet
+}
+
+var counter int
+
+var epMap struct {
+	sync.Mutex
+	m map[string]*epVal
+}
+
+var nwMap struct {
+	sync.Mutex
+	m map[string]*nwVal
+}
+
+var dbFile string
+var db *bolt.DB
+
+func init() {
+	epMap.m = make(map[string]*epVal)
+	nwMap.m = make(map[string]*nwVal)
+	dbFile = "/tmp/ovsdpdk_bolt.db"
+}
+
+//We should never see any errors in this function
+func sendResponse(resp interface{}, w http.ResponseWriter) {
+	rb, err := json.Marshal(resp)
+	if err != nil {
+		glog.Errorf("unable to marshal response %v", err)
+	}
+	glog.Infof("Sending response := %v, %v", resp, err)
+	fmt.Fprintf(w, "%s", rb)
+	return
+}
+
+func getBody(r *http.Request) ([]byte, error) {
+	body, err := ioutil.ReadAll(r.Body)
+	glog.Infof("URL [%s] Body [%s] Error [%v]", r.URL.Path[1:], string(body), err)
+	return body, err
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	body, _ := getBody(r)
+	resp := api.Response{}
+	resp.Err = "Unhandled API request " + string(r.URL.Path[1:]) + " " + string(body)
+	sendResponse(resp, w)
+}
+
+func handlerPluginActivate(w http.ResponseWriter, r *http.Request) {
+	_, _ = getBody(r)
+	//TODO: Where is this encoding?
+	resp := `{
+    "Implements": ["NetworkDriver", "IpamDriver"]
+}`
+	fmt.Fprintf(w, "%s", resp)
+}
+
+func handlerGetCapabilities(w http.ResponseWriter, r *http.Request) {
+	_, _ = getBody(r)
+	resp := api.GetCapabilityResponse{Scope: "local"}
+	sendResponse(resp, w)
+}
+
+func handlerCreateNetwork(w http.ResponseWriter, r *http.Request) {
+	resp := api.CreateNetworkResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.CreateNetworkRequest{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	//TODO: We can auto generate this, in the future. Just needs to be unique
+	v, ok := req.Options["com.docker.network.generic"].(map[string]interface{})
+	if !ok {
+		resp.Err = "Error: network options incorrect or unspecified. Please provide bridge info"
+		sendResponse(resp, w)
+		return
+	}
+
+	bridge, ok := v["bridge"].(string)
+	if !ok {
+		resp.Err = "Error: network incorrect or unspecified. Please provide bridge info"
+		sendResponse(resp, w)
+		return
+	}
+
+	nwMap.Lock()
+	defer nwMap.Unlock()
+
+	//Record the docker network UUID to SDN bridge mapping
+	//This has to survive a plugin crash/restart and needs to be persisted
+	nwMap.m[req.NetworkID] = &nwVal{
+		Bridge:  bridge,
+		Gateway: *req.IPv4Data[0].Gateway,
+	}
+
+	if err := dbAdd("nwMap", req.NetworkID, nwMap.m[req.NetworkID]); err != nil {
+		glog.Errorf("Unable to update db %v", err)
+	}
+	//TODO: Create the ovs bridge for this network
+	//ovs-vsctl add-br br0 -- set bridge $bridge datapath_type=netdev
+
+	sendResponse(resp, w)
+}
+
+func handlerDeleteNetwork(w http.ResponseWriter, r *http.Request) {
+	resp := api.DeleteNetworkResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.DeleteNetworkRequest{}
+	if err = json.Unmarshal(body, &req); err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	glog.Infof("Delete Network := %v", req.NetworkID)
+
+	nwMap.Lock()
+	defer nwMap.Unlock()
+
+	bridge := nwMap.m[req.NetworkID].Bridge
+	delete(nwMap.m, req.NetworkID)
+	if err := dbDelete("nwMap", req.NetworkID); err != nil {
+		glog.Errorf("Unable to update db %v %v", err, bridge)
+	}
+
+	//TODO: Delete the ovs bridge
+	//ovs-vsctl del-br $bridge
+
+	sendResponse(resp, w)
+	return
+}
+
+func handlerEndpointOperInfof(w http.ResponseWriter, r *http.Request) {
+	resp := api.EndpointInfoResponse{}
+	body, err := getBody(r)
+
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.EndpointInfoRequest{}
+	err = json.Unmarshal(body, &req)
+
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	sendResponse(resp, w)
+}
+
+func handlerCreateEndpoint(w http.ResponseWriter, r *http.Request) {
+	resp := api.CreateEndpointResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.CreateEndpointRequest{}
+	if err = json.Unmarshal(body, &req); err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	if req.Interface.Address == "" {
+		resp.Err = "Error: IP Address parameter not provided in docker run"
+		sendResponse(resp, w)
+		return
+	}
+
+	ip, _, err := net.ParseCIDR(req.Interface.Address)
+	if err != nil {
+		resp.Err = "Error: Invalid IP Address " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	nwMap.Lock()
+	bridge := nwMap.m[req.NetworkID].Bridge
+	nwMap.Unlock()
+
+	if bridge == "" {
+		resp.Err = "Error: incompatible network"
+		sendResponse(resp, w)
+		return
+	}
+
+	nwMap.Lock()
+	defer nwMap.Unlock()
+
+	epMap.Lock()
+	defer epMap.Unlock()
+
+	//Generate a unique ovs port name
+	counter++
+	ovsDpdkPort := fmt.Sprintf("ovd_%d", counter)
+
+	cmd := "ovs-vsctl"
+	args := []string{"add-port", bridge, ovsDpdkPort, "--", "set", "Interface", ovsDpdkPort, "type=dpdkvhostuser"}
+	_, err = exec.Command(cmd, args...).Output()
+	if err != nil {
+		glog.Infof("ERROR: [%v] [%v] [%v] ", cmd, args, err)
+		resp.Err = fmt.Sprintf("Error EndPointCreate: [%v] [%v] [%v]",
+			cmd, args, err)
+		sendResponse(resp, w)
+		return
+	}
+	glog.Infof("Created ovs port %v %v %v", bridge, ovsDpdkPort, err)
+
+	epMap.m[req.EndpointID] = &epVal{
+		IP:      req.Interface.Address,
+		ovsPort: ovsDpdkPort,
+	}
+
+	if err := dbAdd("epMap", req.EndpointID, epMap.m[req.EndpointID]); err != nil {
+		glog.Errorf("Unable to update db %v %v", err, ip)
+	}
+	if err := dbAdd("global", "counter", counter); err != nil {
+		glog.Errorf("Unable to update db %v", err)
+	}
+
+	/* Setup the dummy interface corresponding to the dpdk port
+	 * This is done so that docker CNM will program the IP Address
+	 * and other properties on this Interface
+	 * This dummy interface will be discovered by clear containers
+	 * which then maps the actual ovs port to the VM
+	 * This is needed today as docker does not pass any information
+	 * passed to it from the network plugin to the runtime
+	 */
+	cmd = "ip"
+	args = []string{"link", "add", ovsDpdkPort, "type", "dummy"}
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		resp.Err = fmt.Sprintf("Error EndPointCreate: [%v] [%v] [%v]",
+			cmd, args, err)
+		sendResponse(resp, w)
+		return
+	}
+
+	glog.Infof("Setup dummy port %v %v ", cmd, args)
+
+	sendResponse(resp, w)
+}
+
+func handlerDeleteEndpoint(w http.ResponseWriter, r *http.Request) {
+	resp := api.DeleteEndpointResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.DeleteEndpointRequest{}
+	if err = json.Unmarshal(body, &req); err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	epMap.Lock()
+	m := epMap.m[req.EndpointID]
+	delete(epMap.m, req.EndpointID)
+	if err := dbDelete("epMap", req.EndpointID); err != nil {
+		glog.Errorf("Unable to update db %v %v", err, m)
+	}
+	epMap.Unlock()
+
+	//TODO: Delete the ovs port and the dummy interface
+
+	sendResponse(resp, w)
+}
+
+func handlerJoin(w http.ResponseWriter, r *http.Request) {
+	resp := api.JoinResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.JoinRequest{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	nwMap.Lock()
+	epMap.Lock()
+	nm := nwMap.m[req.NetworkID]
+	em := epMap.m[req.EndpointID]
+	nwMap.Unlock()
+	epMap.Unlock()
+
+	resp.Gateway = nm.Gateway.IP.String()
+	resp.InterfaceName = &api.InterfaceName{
+		SrcName:   em.ovsPort,
+		DstPrefix: "eth",
+	}
+	glog.Infof("Join Response %v %v", resp, em.ovsPort)
+	sendResponse(resp, w)
+}
+
+func handlerLeave(w http.ResponseWriter, r *http.Request) {
+	resp := api.LeaveResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.LeaveRequest{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	sendResponse(resp, w)
+}
+
+func handlerDiscoverNew(w http.ResponseWriter, r *http.Request) {
+	resp := api.DiscoveryResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.DiscoveryNotification{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	sendResponse(resp, w)
+}
+
+func handlerDiscoverDelete(w http.ResponseWriter, r *http.Request) {
+	resp := api.DiscoveryResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.DiscoveryNotification{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	sendResponse(resp, w)
+}
+
+func handlerExternalConnectivity(w http.ResponseWriter, r *http.Request) {
+	resp := api.ProgramExternalConnectivityResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.ProgramExternalConnectivityRequest{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	sendResponse(resp, w)
+}
+
+func handlerRevokeExternalConnectivity(w http.ResponseWriter, r *http.Request) {
+	resp := api.RevokeExternalConnectivityResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := api.RevokeExternalConnectivityResponse{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Err = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	sendResponse(resp, w)
+}
+
+func ipamGetCapabilities(w http.ResponseWriter, r *http.Request) {
+	if _, err := getBody(r); err != nil {
+		glog.Infof("ipamGetCapabilities: unable to get request body [%v]", err)
+	}
+	resp := ipamapi.GetCapabilityResponse{RequiresMACAddress: true}
+	sendResponse(resp, w)
+}
+
+func ipamGetDefaultAddressSpaces(w http.ResponseWriter, r *http.Request) {
+	resp := ipamapi.GetAddressSpacesResponse{}
+	if _, err := getBody(r); err != nil {
+		glog.Infof("ipamGetDefaultAddressSpaces: unable to get request body [%v]", err)
+	}
+
+	resp.GlobalDefaultAddressSpace = ""
+	resp.LocalDefaultAddressSpace = ""
+	sendResponse(resp, w)
+}
+
+func ipamRequestPool(w http.ResponseWriter, r *http.Request) {
+	resp := ipamapi.RequestPoolResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Error = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := ipamapi.RequestPoolRequest{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Error = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	resp.PoolID = uuid.Generate().String()
+	resp.Pool = req.Pool
+	sendResponse(resp, w)
+}
+
+func ipamReleasePool(w http.ResponseWriter, r *http.Request) {
+	resp := ipamapi.ReleasePoolResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Error = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := ipamapi.ReleasePoolRequest{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Error = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	sendResponse(resp, w)
+}
+
+func ipamRequestAddress(w http.ResponseWriter, r *http.Request) {
+	resp := ipamapi.RequestAddressResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Error = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := ipamapi.RequestAddressRequest{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Error = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	//TODO: Should come from the subnet mask for the subnet
+	if req.Address != "" {
+		resp.Address = req.Address + "/24"
+	} else {
+		resp.Error = "Error: Request does not have IP address. Specify using --ip"
+	}
+	sendResponse(resp, w)
+}
+
+func ipamReleaseAddress(w http.ResponseWriter, r *http.Request) {
+	resp := ipamapi.ReleaseAddressResponse{}
+
+	body, err := getBody(r)
+	if err != nil {
+		resp.Error = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	req := ipamapi.ReleaseAddressRequest{}
+	if err := json.Unmarshal(body, &req); err != nil {
+		resp.Error = "Error: " + err.Error()
+		sendResponse(resp, w)
+		return
+	}
+
+	sendResponse(resp, w)
+}
+
+func dbTableInit(tables []string) (err error) {
+
+	glog.Infof("dbInit Tables := %v", tables)
+	for i, v := range tables {
+		glog.Infof("table[%v] := %v, %v", i, v, []byte(v))
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		for _, table := range tables {
+			_, err := tx.CreateBucketIfNotExists([]byte(table))
+			if err != nil {
+				return fmt.Errorf("Bucket creation error: %v %v", table, err)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		glog.Errorf("Table creation error %v", err)
+	}
+
+	return err
+}
+
+func dbAdd(table string, key string, value interface{}) (err error) {
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		var v bytes.Buffer
+
+		if err := gob.NewEncoder(&v).Encode(value); err != nil {
+			glog.Errorf("Encode Error: %v %v", err, value)
+			return err
+		}
+
+		bucket := tx.Bucket([]byte(table))
+		if bucket == nil {
+			return fmt.Errorf("Bucket %v not found", table)
+		}
+
+		err = bucket.Put([]byte(key), v.Bytes())
+		if err != nil {
+			return fmt.Errorf("Key Store error: %v %v %v %v", table, key, value, err)
+		}
+		return nil
+	})
+
+	return err
+}
+
+func dbDelete(table string, key string) (err error) {
+
+	err = db.Update(func(tx *bolt.Tx) error {
+
+		bucket := tx.Bucket([]byte(table))
+		if bucket == nil {
+			return fmt.Errorf("Bucket %v not found", table)
+		}
+
+		err = bucket.Delete([]byte(key))
+		if err != nil {
+			return fmt.Errorf("Key Delete error: %v %v ", key, err)
+		}
+		return nil
+	})
+
+	return err
+}
+
+func dbGet(table string, key string) (value interface{}, err error) {
+
+	err = db.View(func(tx *bolt.Tx) error {
+
+		bucket := tx.Bucket([]byte(table))
+		if bucket == nil {
+			return fmt.Errorf("Bucket %v not found", table)
+		}
+
+		val := bucket.Get([]byte(key))
+		if val == nil {
+			return nil
+		}
+
+		v := bytes.NewReader(val)
+		if err := gob.NewDecoder(v).Decode(value); err != nil {
+			glog.Errorf("Decode Error: %v %v %v", table, key, err)
+			return err
+		}
+
+		return nil
+	})
+
+	return value, err
+}
+
+func initDb() error {
+
+	options := bolt.Options{
+		Timeout: 3 * time.Second,
+	}
+
+	var err error
+	db, err = bolt.Open(dbFile, 0644, &options)
+	if err != nil {
+		return fmt.Errorf("dbInit failed %v", err)
+	}
+
+	tables := []string{"global", "nwMap", "epMap"}
+	if err := dbTableInit(tables); err != nil {
+		return fmt.Errorf("dbInit failed %v", err)
+	}
+
+	c, err := dbGet("global", "counter")
+	if err != nil {
+		glog.Errorf("dbGet failed %v", err)
+		counter = 100
+	} else {
+		var ok bool
+		counter, ok = c.(int)
+		if !ok {
+			counter = 100
+		}
+	}
+
+	err = db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("nwMap"))
+
+		err := b.ForEach(func(k, v []byte) error {
+			vr := bytes.NewReader(v)
+			nVal := &nwVal{}
+			if err := gob.NewDecoder(vr).Decode(nVal); err != nil {
+				return fmt.Errorf("Decode Error: %v %v %v", string(k), string(v), err)
+			}
+			nwMap.m[string(k)] = nVal
+			glog.Infof("nwMap key=%v, value=%v\n", string(k), nVal)
+			return nil
+		})
+		return err
+	})
+
+	if err != nil {
+		return err
+	}
+
+	err = db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("epMap"))
+
+		err := b.ForEach(func(k, v []byte) error {
+			vr := bytes.NewReader(v)
+			eVal := &epVal{}
+			if err := gob.NewDecoder(vr).Decode(eVal); err != nil {
+				return fmt.Errorf("Decode Error: %v %v %v", string(k), string(v), err)
+			}
+			epMap.m[string(k)] = eVal
+			glog.Infof("epMap key=%v, value=%v\n", string(k), eVal)
+			return nil
+		})
+		return err
+	})
+
+	return err
+}
+
+func main() {
+	flag.Parse()
+
+	if err := initDb(); err != nil {
+		glog.Fatalf("db init failed, quitting [%v]", err)
+	}
+	defer func() {
+		err := db.Close()
+		glog.Errorf("unable to close database [%v]", err)
+	}()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/Plugin.Activate", handlerPluginActivate)
+	r.HandleFunc("/NetworkDriver.GetCapabilities", handlerGetCapabilities)
+	r.HandleFunc("/NetworkDriver.CreateNetwork", handlerCreateNetwork)
+	r.HandleFunc("/NetworkDriver.DeleteNetwork", handlerDeleteNetwork)
+	r.HandleFunc("/NetworkDriver.CreateEndpoint", handlerCreateEndpoint)
+	r.HandleFunc("/NetworkDriver.DeleteEndpoint", handlerDeleteEndpoint)
+	r.HandleFunc("/NetworkDriver.EndpointOperInfo", handlerEndpointOperInfof)
+	r.HandleFunc("/NetworkDriver.Join", handlerJoin)
+	r.HandleFunc("/NetworkDriver.Leave", handlerLeave)
+	r.HandleFunc("/NetworkDriver.DiscoverNew", handlerDiscoverNew)
+	r.HandleFunc("/NetworkDriver.DiscoverDelete", handlerDiscoverDelete)
+	r.HandleFunc("/NetworkDriver.ProgramExternalConnectivity", handlerExternalConnectivity)
+	r.HandleFunc("/NetworkDriver.RevokeExternalConnectivity", handlerRevokeExternalConnectivity)
+
+	r.HandleFunc("/IpamDriver.GetCapabilities", ipamGetCapabilities)
+	r.HandleFunc("/IpamDriver.GetDefaultAddressSpaces", ipamGetDefaultAddressSpaces)
+	r.HandleFunc("/IpamDriver.RequestPool", ipamRequestPool)
+	r.HandleFunc("/IpamDriver.ReleasePool", ipamReleasePool)
+	r.HandleFunc("/IpamDriver.RequestAddress", ipamRequestAddress)
+
+	r.HandleFunc("/", handler)
+	err := http.ListenAndServe("127.0.0.1:9075", r)
+	if err != nil {
+		glog.Errorf("docker plugin http server failed, [%v]", err)
+	}
+}
